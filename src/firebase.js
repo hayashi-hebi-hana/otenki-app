4f759745082e59e9e53f86f4fc50491b4b2ca233
junseinagao <junsei.nagao@gmail.com>
2021-08-21 17:03:20 +0900

    firebaseを追加

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import firebase from "firebase"
+import "firebase/firestore"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCu-8tl-G3HcwUBbwlH0vLxmv2VRNtvxFA",
+  authDomain: "otenki-app.firebaseapp.com",
+  projectId: "otenki-app",
+  storageBucket: "otenki-app.appspot.com",
+  messagingSenderId: "982741373754",
+  appId: "1:982741373754:web:bd0238f136025c68eff603",
+}
+
+firebase.initializeApp(firebaseConfig)
