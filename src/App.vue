e0831e7ec9770cdf6f8742f2128e6cb6d57d0c99
iron-folgore <beringei884@outlook.jp>
2021-08-27 15:37:33 +0900

    読み込み時に天気を表示

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link> |
-      <router-link to="/otenkiAPI">OtenkiAPI</router-link>
+      <router-link to="/otenkiAPI">OtenkiAPI</router-link> |
       <router-link to="/image-post">ImagePost</router-link>
     </div>
     <router-view />
