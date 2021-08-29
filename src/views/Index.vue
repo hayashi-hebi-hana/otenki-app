f2daecdad55b8ba5fe831ff595ae63e6fd4b700f
jasminetdoll <jasminetflower2109@gmail.com>
2021-08-29 13:53:14 +0900

    Index.vueにデータを追加、画像の表示はImageURLが追加されたらします

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,3 +1,32 @@
 <template>
-  <section>メインページ</section>
+  <div>
+    {{ allData }}
+  </div>
 </template>
+
+<script>
+import firebase from "firebase"
+
+export default {
+  data() {
+    return {
+      allData: [],
+    }
+  },
+  created() {
+    {
+      firebase
+        .firestore()
+        .collection("clothes")
+        .get()
+        .then((snapshot) => {
+          snapshot.forEach((doc) => {
+            this.allData.push(doc.data())
+          })
+        })
+    }
+  },
+  methods: {},
+  computed: {},
+}
+</script>
