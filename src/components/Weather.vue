0f8006a95b41e5bf2921fe61ee06170fdb183aab
iron-folgore <beringei884@outlook.jp>
2021-08-31 11:06:48 +0900

    css途中です

--- src/components/Weather.vue
+++ src/components/Weather.vue
@@ -3,7 +3,19 @@
     <h1>天気取得API</h1>
     <div class="tenki-hyouji">
       <div>↓現在地の体感温度、湿度、天気を下に表示↓</div>
-      <div id="result"></div>
+      <div id="result">
+        <div id="weather">
+          <div id="description"></div>
+          <img src="" id="icon">
+        </div>
+        <div id="temp">
+          <div id="kion"></div>
+          <div id="taikan"></div>
+        </div>
+        <div id="humidity"><p id="humidText"></p></div>
+      </div> 
+      <!-- 温度(気温と体感温度)と湿度と天気を表示したらよい？
+      服って一日中着るもんやから現在の情報でなく一日の情報総合すべきなのでは……？ -->
     </div>
   </div>
 </template>
@@ -16,7 +28,6 @@ export default {
         "https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid=1b9025d02ac2423f55483b43946ed204&lang=ja&units=metric",
     }
   },
-  methods: {},
   created: function () {
     navigator.geolocation.getCurrentPosition((position) => {
       const newURL =
@@ -33,15 +44,20 @@ export default {
         })
         .then((data) => {
           console.log(data)
-          const kekka = document.getElementById("result")
-          kekka.textContent =
-            "現在地の体感温度は" +
-            data.current.feels_like +
-            "℃、湿度は" +
-            data.current.humidity +
-            "%、天気は" +
-            data.current.weather[0].description +
-            "です。"
+          // const kekka = document.getElementById("result")
+          // kekka.textContent =
+          //   "現在地の体感温度は" +
+          //   data.current.feels_like +
+          //   "℃、湿度は" +
+          //   data.current.humidity +
+          //   "%、天気は" +
+          //   data.current.weather[0].description +
+          //   "です。"
+          document.getElementById('icon').src= "http://openweathermap.org/img/w/" + data.current.weather[0].icon + ".png"
+          document.getElementById('description').textContent= "現在の天気：" + data.current.weather[0].description
+          document.getElementById('kion').textContent= "現在の気温：" + data.current.dew_point + "℃"
+          document.getElementById('taikan').textContent= "体感温度：" + data.current.feels_like + "℃"
+          document.getElementById('humidText').textContent= "現在の湿度：" + data.current.humidity + "%"
         })
     })
   },
@@ -51,5 +67,30 @@ export default {
 <style scoped>
 .tenki-hyouji {
   margin-top: 1.3rem;
+}
+
+#result {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+#weather, #temp, #humidity {
+  height: 5rem;
+  width: 24%;
+  padding: 0.5rem;
+  color: white;
+  background-color: slategray;
+}
+
+#weather, #temp {
+  border-right: 0.1rem white solid;
+}
+
+#humidText {
+  margin-block-start: 0rem;
+  margin-block-end: 0rem;
+  text-align:center;
+  color: white;
 }
 </style>
