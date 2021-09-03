6ae86d0491389baa70f3eed43602ad6b992c6813
iron-folgore <beringei884@outlook.jp>
2021-09-03 18:08:03 +0900

    css途中です

--- src/components/Weather.vue
+++ src/components/Weather.vue
@@ -5,14 +5,32 @@
       <div>↓現在地の体感温度、湿度、天気を下に表示↓</div>
       <div id="result">
         <div id="weather">
-          <div id="description">現在の天気：{{ description }}</div>
-          <img src="" id="icon" />
+          <div class="tenki-wrapper">
+            <div id="description">
+              <span class="bold">現在の天気</span><br />
+              {{ description }}
+            </div>
+            <img src="" id="icon" />
+          </div>
         </div>
         <div id="temp">
-          <div id="kion"></div>
-          <div id="taikan"></div>
+          <div class="temp-wrapper">
+            <div id="kion">
+              <span class="bold">現在の気温</span>
+              {{ kion }}℃
+            </div>
+            <div id="taikan">
+              <span class="bold">体感気温</span>
+              {{ taikan }}℃
+            </div>
+          </div>
         </div>
-        <div id="humidity"><p id="humidText"></p></div>
+        <div id="humidity">
+          <p id="humidText">
+            <span class="bold">現在の湿度</span>
+            {{ sitsudo }}%
+          </p>
+        </div>
       </div>
       <!-- 温度(気温と体感温度)と湿度と天気を表示したらよい？
       服って一日中着るもんやから現在の情報でなく一日の情報総合すべきなのでは……？ -->
@@ -25,6 +43,9 @@ export default {
   data() {
     return {
       description: "",
+      kion: "",
+      taikan: "",
+      sitsudo: "",
       callURL:
         "https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid=1b9025d02ac2423f55483b43946ed204&lang=ja&units=metric",
     }
@@ -49,8 +70,12 @@ export default {
           return res.json()
         })
         .then((data) => {
-          this.sendData(data)
           this.description = data.current.weather[0].description
+          this.kion = data.current.dew_point
+          this.taikan = data.current.feels_like
+          this.sitsudo = data.current.humidity
+          this.sendData(data)
+          console.log(data)
           // const kekka = document.getElementById("result")
           // kekka.textContent =
           //   "現在地の体感温度は" +
@@ -66,12 +91,12 @@ export default {
             ".png"
           // document.getElementById("description").textContent =
           //   "現在の天気：" + data.current.weather[0].description
-          document.getElementById("kion").textContent =
-            "現在の気温：" + data.current.dew_point + "℃"
-          document.getElementById("taikan").textContent =
-            "体感温度：" + data.current.feels_like + "℃"
-          document.getElementById("humidText").textContent =
-            "現在の湿度：" + data.current.humidity + "%"
+          // document.getElementById("kion").textContent =
+          //   "現在の気温：" + data.current.dew_point + "℃"
+          // document.getElementById("taikan").textContent =
+          //   "体感温度：" + data.current.feels_like + "℃"
+          // document.getElementById("humidText").textContent =
+          //   "現在の湿度：" + data.current.humidity + "%"
         })
     })
   },
@@ -92,11 +117,12 @@ export default {
 #weather,
 #temp,
 #humidity {
-  height: 5rem;
+  height: 7rem;
   width: 24%;
   padding: 0.5rem;
   color: white;
   background-color: slategray;
+  position: relative;
 }
 
 #weather,
@@ -104,10 +130,40 @@ export default {
   border-right: 0.1rem white solid;
 }
 
+.tenki-wrapper,
+.temp-wrapper {
+  width: 80%;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translateY(-50%) translateX(-50%);
+}
+
+/* #kion,
+#taikan,
+#description,
+#icon {
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  margin: auto;
+} */
+
+.bold::after {
+  white-space: pre;
+}
+
 #humidText {
+  width: 80%;
   margin-block-start: 0rem;
   margin-block-end: 0rem;
   text-align: center;
   color: white;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translateY(-50%) translateX(-50%);
 }
 </style>
