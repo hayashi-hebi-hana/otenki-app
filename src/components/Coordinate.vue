8c5316635b3be3d0a632ddeef5e20bbe14632040
junseinagao <junsei.nagao@gmail.com>
2021-09-06 10:26:15 +0900

    some fix

--- src/components/Coordinate.vue
+++ src/components/Coordinate.vue
@@ -7,21 +7,28 @@
 
 <script>
 export default {
-  props: [
-    "clothes",
-    "weather",
-    "color",
-    "selectedSex",
-    "season",
-    "temperature",
-  ],
+  props: ["clothes", "weather", "color", "selectedSex", "season"],
   data() {
     return {
       recommendedClothes: [],
+      temperature: "",
     }
   },
   methods: {
+    getTemperature() {
+      const temp = this.weather.current.feels_like
+      if (temp >= 28) {
+        this.temperature = "hot"
+      } else if (temp >= 23) {
+        this.temperature = "warm"
+      } else if (temp >= 15) {
+        this.temperature = "comfortable"
+      } else {
+        this.temperature = "cold"
+      }
+    },
     getCloth() {
+      this.getTemperature()
       // this.clothes から sex と weather と color が 一致してる アイテムだけを取り出した recommendedClothes を作る
       // for文とif 文で 条件に当てはまるものを temp に入れてく
       let temp = []
@@ -33,6 +40,7 @@ export default {
           this.clothes[i].color === this.color &&
           // 今のtemperatureとseasonの情報を取得したい
           this.clothes[i].season === this.season &&
+          // temperature は
           this.clothes[i].temperature === this.temperature
         ) {
           temp.push(this.clothes[i])
